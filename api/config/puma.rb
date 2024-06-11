2f9faede120088ea85c9bc5fb2f6fc19fd5cbcb8
q4c-tsuchiya <ryota.tsuchiya.231@gmail.com>
2024-06-11 13:36:21 +0900

    heroku 導入

--- api/config/puma.rb
+++ api/config/puma.rb
@@ -31,8 +31,21 @@ pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
 # This directive tells Puma to first boot the application and load code
 # before forking the application. This takes advantage of Copy On Write
 # process behavior so workers use less memory.
-#
-# preload_app!
+workers Integer(ENV.fetch("WEB_CONCURRENCY") { 2 })
+
+max_threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }
+min_threads_count = ENV.fetch("RAILS_MIN_THREADS") { max_threads_count }
+threads min_threads_count, max_threads_count
+
+preload_app!
+
+rackup      DefaultRackup
+port        ENV.fetch("PORT") { 3000 }
+environment ENV.fetch("RAILS_ENV") { "development" }
+
+on_worker_boot do
+  ActiveRecord::Base.establish_connection
+end
 
 # Allow puma to be restarted by `rails restart` command.
 plugin :tmp_restart
