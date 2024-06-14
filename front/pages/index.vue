1b6370baea16e0ef21e844201270e71ec1806ef2
q4c-tsuchiya <ryota.tsuchiya.231@gmail.com>
2024-06-14 15:04:15 +0900

    axiosの基礎動作検証

--- front/pages/index.vue
+++ front/pages/index.vue
@@ -4,18 +4,16 @@ div
   div(v-for="(msg, i) in msgs" :key="i") {{ msg }}
 </template>
 
-<script>
-export default {
-  data () {
-    return {
-      msgs: []
-    }
-  },
-  methods: {
-    getMsg () {
-      this.$axios.$get('/hello')
-        .then(res => this.msgs.push(res))
-    }
+<script setup>
+import { ref } from 'vue'
+import axios from 'axios'
+
+const msgs = ref(['a', 'b'])
+
+async function getMsg () {
+  const res = await axios.get('http://localhost:3000/api/v1/skills/index')
+  console.log(res)
+  msgs.value = res.data.skills
+  console.log(msgs)
   }
-}
 </script>
