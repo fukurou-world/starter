2e6b69695aaea61ca859139ce346c8c5f85fb6b2
q4c-tsuchiya <ryota.tsuchiya.231@gmail.com>
2024-06-16 13:07:37 +0900

    Docker調整（yarm禁止、dockerignore

--- spa/app.vue
+++ spa/app.vue
@@ -1,6 +1,16 @@
-<template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
-  </div>
-</template>
+<template lang="pug">
+  div
+    button(type="button" name="button" @click="getMsg") RailsからAPIを取得する
+    div(v-for="(msg, i) in msgs" :key="i") {{ msg }}
+  </template>
+  
+  <script setup>
+  import { ref } from 'vue'
+  
+  const msgs = ref(['a', 'b'])
+  
+  async function getMsg () {
+    const msgs = ['sss']
+    console.log(msgs)
+    }
+  </script>
